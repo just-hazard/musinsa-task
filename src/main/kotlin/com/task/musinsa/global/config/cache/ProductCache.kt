4fcf97c5702d9e08b08cffe6b62fb52a0033d676
just-hazard <edenhazard5870@gmail.com>
2022-09-05 13:09:04 +0900

    feat - cache 설정

---
+++ src/main/kotlin/com/task/musinsa/global/config/cache/ProductCache.kt
@@ -0,0 +1,24 @@
+package com.task.musinsa.global.config.cache
+
+import com.task.musinsa.application.InitProductCacheService
+import com.task.musinsa.repositories.CategoryRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class ProductCache : CommandLineRunner {
+
+    @Autowired
+    private lateinit var categoryRepository: CategoryRepository
+
+    @Autowired
+    private lateinit var initProductCacheService: InitProductCacheService
+
+    override fun run(vararg args: String?) {
+        categoryRepository.findAll()
+            .forEach {
+                initProductCacheService.findAllProducts(it.id)
+            }
+    }
+}
